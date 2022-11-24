27d5404356feba3f45af54493b0a231119d2cc9c
Dakota Greenlee <dg2120032@gmail.com>
2022-11-23 20:02:02 -0800

    fixed icon and polygons

--- public/app.js
+++ public/app.js
@@ -17,17 +17,19 @@ marker.addTo(myMap).bindPopup('<p1><b>The Hoxton, Paris</b></p1>').openPopup()
 
 
 // draw the 2nd arrondissement                                          
-const latLngs = [[48.863368120198004, 2.3509079846928516],
-[48.86933262048345, 2.3542531602919805],
-[48.87199261164275, 2.3400569901592183],
-[48.86993336274516, 2.3280142476578813], 
-[48.86834104280146, 2.330308418109664]]
+const polygon = L.polygon([
+    [48.863368120198004, 2.3509079846928516],
+    [48.86933262048345, 2.3542531602919805],
+    [48.87199261164275, 2.3400569901592183],
+    [48.86993336274516, 2.3280142476578813], 
+    [48.86834104280146, 2.330308418109664]
+]).addTo(myMap);
 
 
 
 // create red pin marker
 const redPin = L.icon({
-    iconUrl: './public/assets/red-pin.png',
+    iconUrl:  './assets/red-pin.png',
     iconSize:     [38, 38], // size of the icon
     iconAnchor:   [19, 38], // point of the icon which will correspond to marker's location
     popupAnchor:  [0, -38] // point from which the popup should open relative to the iconAnchor
